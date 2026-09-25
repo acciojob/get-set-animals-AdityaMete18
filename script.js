44e736647a8c0aad890d2fa49f4a66258eced219
Aditya Mete <167237391+AdityaMete18@users.noreply.github.com>
2026-09-26 02:36:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //complete this code
 class Animal {
 	constructor(species){
-		this.species = species
+		this._species = species
 	}
 
 	get species(){
-		return species;
+		return _species;
 	}
 	makeSound(){
 		console.log(`The ${this.species} makes a sound`);
